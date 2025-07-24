04ba97586fa3881f821e671b9508d38572cea9f5
Abdulbaki Yakar <abdulbaki.yakar0@gmail.com>
2025-07-25 01:51:18 +0300

    Try-catch bloğu uygulandı ve işlemler düzenlendi

--- HesapMakinesi/Form1.cs
+++ HesapMakinesi/Form1.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace HesapMakinesi
 {
     public partial class Form1 : Form
@@ -9,18 +11,33 @@ namespace HesapMakinesi
 
         private void button4_Click(object sender, EventArgs e)
         {
+            try
+            {
+                double s1 = Convert.ToDouble(txtSayi1.Text);
+                double s2 = Convert.ToDouble(txtSayi2.Text);
+                if (s2 != 0)
+                {
+                    double sonuc = s1 / s2;
+                    lstSonuclar.Items.Add($"{s1} / {s2} = {sonuc}");
+                }
+                else if ((s2 == 0) && (s1 == 0))
+                {
+                    lstSonuclar.Items.Add("BELÝRSÝZ");
+                }
+                else if (s2 == 0)
+                {
+                    lstSonuclar.Items.Add("HATA: Sayý sýfýra bölünmez TANIMSIZ!!!");
+                }
+                
+               
+            }
+            catch (Exception ex)
+            {
 
-            double s1 = Convert.ToDouble(txtSayi1.Text);
-            double s2 = Convert.ToDouble(txtSayi2.Text);
-            if (s2 != 0)
-            {
-                double sonuc = s1 / s2;
-                lstSonuclar.Items.Add($"{s1} / {s2} = {sonuc}");
+                Console.WriteLine("HATA:" + ex.Message);
             }
-            else
-            {
-                lstSonuclar.Items.Add("HATA: Sayý sýfýra bölünmez");
-            }
+
+          
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -30,26 +47,54 @@ namespace HesapMakinesi
 
         private void btnTopla_Click(object sender, EventArgs e)
         {
-            double s1 = Convert.ToDouble(txtSayi1.Text);
-            double s2 = Convert.ToDouble(txtSayi2.Text);
-            double sonuc = s1 + s2;
-            lstSonuclar.Items.Add($"{s1} + {s2} = {sonuc}");
+            try
+            {
+                double s1 = Convert.ToDouble(txtSayi1.Text);
+                double s2 = Convert.ToDouble(txtSayi2.Text);
+                double sonuc = s1 + s2;
+                lstSonuclar.Items.Add($"{s1} + {s2} = {sonuc}");
+            }
+            catch (Exception ex)
+            {
+
+                 Console.WriteLine("HATA:" +ex.Message);
+            }
+            
         }
 
         private void btnCikarma_Click(object sender, EventArgs e)
         {
-            double s1 = Convert.ToDouble(txtSayi1.Text);
-            double s2 = Convert.ToDouble(txtSayi2.Text);
-            double sonuc = s1 - s2;
-            lstSonuclar.Items.Add($"{s1} - {s2} = {sonuc}");
+            try
+            {
+                double s1 = Convert.ToDouble(txtSayi1.Text);
+                double s2 = Convert.ToDouble(txtSayi2.Text);
+                double sonuc = s1 - s2;
+                lstSonuclar.Items.Add($"{s1} - {s2} = {sonuc}");
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("HATA: "+ex.Message);
+            }
+           
         }
 
         private void btnCarpma_Click(object sender, EventArgs e)
         {
-            double s1 = Convert.ToDouble(txtSayi1.Text);
-            double s2 = Convert.ToDouble(txtSayi2.Text);
-            double sonuc = s1 * s2;
-            lstSonuclar.Items.Add($"{s1} * {s2} = {sonuc}");
+            try
+            {
+                double s1 = Convert.ToDouble(txtSayi1.Text);
+                double s2 = Convert.ToDouble(txtSayi2.Text);
+                double sonuc = s1 * s2;
+                lstSonuclar.Items.Add($"{s1} * {s2} = {sonuc}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine( "HATA:" + ex.Message);
+            }
+            
         }
 
         private void btnTemizle_Click(object sender, EventArgs e)
--- HesapMakinesi/obj/Debug/net8.0-windows/HesapMakinesi.AssemblyInfo.cs
+++ HesapMakinesi/obj/Debug/net8.0-windows/HesapMakinesi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HesapMakinesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a74c9deab882dfce97914c06f66dceb67ff2af3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HesapMakinesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HesapMakinesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
